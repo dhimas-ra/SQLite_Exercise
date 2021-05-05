103369884c5ac9bd52906ad4ced1d59ff72292f8
Dhimas <dhimas.ra48@gmail.com>
2021-05-05 07:59:57 +0700

    .

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.sqlite.adapter.TemanAdapter;
 import com.example.sqlite.database.DBController;
@@ -17,13 +26,17 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private TemanAdapter adapter;
-    private ArrayList<Teman> temanArrayList;
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    RecyclerView recyclerView;
+    TemanAdapter adapter;
+    ArrayList<Teman> temanArrayList;
     DBController controller = new DBController(this);
     String id,nm,tlp;
     private FloatingActionButton fab;
+
+    Bundle bundle = new Bundle();
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,32 +46,54 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         fab = findViewById(R.id.floatingBtn);
         BacaData();
-        adapter = new TemanAdapter(temanArrayList);
+        adapter = new TemanAdapter(this, temanArrayList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TemanBaru.class);
+                Intent intent = new Intent(MainActivity.this,TemanBaru.class);
                 startActivity(intent);
             }
         });
+
+
     }
 
     public void BacaData(){
         ArrayList<HashMap<String,String>> daftarTeman = controller.getAllTeman();
         temanArrayList = new ArrayList<>();
-        //memindah dari hasil query kedalam teman
-        for (int i=0;i<daftarTeman.size();i++){
+//        memindahkan dari hasil query kedalam teman
+        for (int i = 0; i<daftarTeman.size();i++){
             Teman teman = new Teman();
 
             teman.setId(daftarTeman.get(i).get("id").toString());
             teman.setNama(daftarTeman.get(i).get("nama").toString());
             teman.setTelpon(daftarTeman.get(i).get("telpon").toString());
-            //pindahkan dari Teman kedalam ArrayList teman di adapter
+
+//          pindahkan dari teman ke dalam Arraylist teman di adapter
             temanArrayList.add(teman);
+
         }
+
+    }
+
+    //    menu item click masuk ke edit data
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.mnedit:
+                intent = new Intent(getApplicationContext(),EditData.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+            case R.id.mnhapus:
+                Toast.makeText(getApplicationContext(),"Ini untuk delete kontak",
+                        Toast.LENGTH_LONG).show();
+                break;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/sqlite/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/sqlite/adapter/TemanAdapter.java
@@ -1,31 +1,49 @@
 package com.example.sqlite.adapter;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sqlite.EditData;
 import com.example.sqlite.R;
 import com.example.sqlite.database.Teman;
 
 import java.util.ArrayList;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    private Context mContext;
     private ArrayList<Teman> listdata;
+    View view;
+    Teman temp;
+    Bundle bundle = new Bundle();
+    Intent intent;
 
-    public TemanAdapter(ArrayList<Teman> listdata) {
+    public TemanAdapter(Context mContext, ArrayList<Teman> listdata)
+    {
+        this.mContext = mContext;
         this.listdata = listdata;
     }
 
     @Override
     public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
-        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        view = LayoutInflater.from(mContext).inflate(R.layout.row_data_teman,parent,false);
         return new TemanViewHolder(view);
     }
 
@@ -40,21 +58,53 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
         holder.namaTxt.setTextSize(20);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
+        holder.menuPopUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(mContext, v);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.show();
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.mnedit:
+                                Toast.makeText(mContext, "Edit!", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.mnhapus:
+                                Toast.makeText(mContext, "Terhapus!", Toast.LENGTH_SHORT).show();
+                                temp = new Teman(listdata.get(position).getNama(), listdata.get(position).getTelpon());
+                                hapusData(position);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+            }
+        });
+    }
+    private void hapusData(int position)
+    {
+        listdata.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, listdata.size());
     }
 
     @Override
     public int getItemCount() {
-        return (listdata != null)?listdata.size() : 0;
+        return listdata.size();
     }
 
     public class TemanViewHolder extends RecyclerView.ViewHolder {
-        private CardView cardku;
+        private CardView cardku, menuPopUp;
         private TextView namaTxt,telponTxt;
         public TemanViewHolder(View view) {
             super(view);
             cardku = (CardView) view.findViewById(R.id.kartuku);
             namaTxt = (TextView) view.findViewById(R.id.textNama);
             telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+            menuPopUp = itemView.findViewById(R.id.kartuku);
         }
+
     }
 }
